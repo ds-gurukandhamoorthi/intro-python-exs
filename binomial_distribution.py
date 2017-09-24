4c756ad612fb8c6ba10c521acea88407a53a7f20
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2017-09-24 21:16:46 +0200

    More

--- binomial_distribution.py
+++ binomial_distribution.py
@@ -1,6 +1,34 @@
-from math import log
+import sys
+from math import log, exp, sqrt
+from gauss import cdf
 
 def log_factorial(n):
     return sum(log(i) for i in range(1,n+1))
+
+def binomial_dist(n, k, p):
+    ln_f = k * log(p) + (n-k) * log(1-p) + log_factorial(n) - log_factorial(k) - log_factorial(n-k)
+    return exp(ln_f)
+
+#FIXME : the following code is not verified
+def binomial_dist_approx(n, k, p):
+    return cdf(k+0.5, n*p, (n*p*sqrt(1-p))) - cdf(k-0.5, n*p, n*p*sqrt(1-p)) 
+
+
+
+if __name__ == "__main__":
+    print(binomial_dist(3, 3, 0.5))
+
+    n = int(sys.argv[1])
+    p = float(sys.argv[2])
+
+    dist = [binomial_dist(n,k,p) for k in range(0,n+1)]
+    print(dist)
+    print(sum(dist))
+
+    dist = [binomial_dist_approx(n,k,p) for k in range(0,n+1)]
+    print(dist)
+    print(sum(dist))
+
+    
 
 
--- couponCollector.py
+++ couponCollector.py
@@ -2,10 +2,13 @@ import argparse
 import random
 import sys
 from numpy.random import choice
+from binomial_distribution import binomial_dist
 
-
-
-def get_coupon(n, penalise=None):
+def get_coupon(n, penalise=None, binomial=False):
+    if binomial == True:
+        probs = [ binomial_dist(n-1, k, 0.5) for k in range(0,n)]
+        assert len(probs) == n
+        return choice(range(n), size=1, p=probs)[0]
     if penalise==None:
         return random.randrange(0,n)
     probs = get_penalised_probs(n, penalise)
@@ -26,12 +29,17 @@ def get_penalised_probs(n, penalise):
 
 
 
-def couponCollector(n):
+def couponCollector(n, penalise=None, binomial=False):
     collected = [False]*n
     steps = 0
     while not all(collected):
         steps += 1
-        samp = get_coupon(n, penalise=[0,1])
+        if binomial == True:
+            samp = get_coupon(n,penalise=None, binomial=True)
+        elif penalise is None: 
+            samp = get_coupon(n, penalise=None, binomial=False)
+        else:
+            samp = get_coupon(n, penalise, binomial=False)
         collected[samp] = True
     return steps
 
@@ -39,15 +47,20 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Estimate number of coupons you need to collect before you obtain each type')
     parser.add_argument('n', type=int, help='number of distinct coupons')
     parser.add_argument('nb_trials', type=int, help='number of times the experiment must be carried on')
+    group=parser.add_mutually_exclusive_group()
+    group.add_argument('--penalise', metavar='N', type=int, nargs='+', help='coupon numbers to penalise ')
+    group.add_argument('--binomial', help='help for ', action='store_true')
+
     args = parser.parse_args()
     n = args.n
     nb_trials = args.nb_trials
+    penalise = args.penalise
+    binomial = args.binomial
 
 
-    nb_trials = int(sys.argv[2])
     totalSteps = 0
     for i in range(0,nb_trials):
-        totalSteps += couponCollector(n)
+        totalSteps += couponCollector(n, penalise, binomial)
 
     print(totalSteps/nb_trials)
 
