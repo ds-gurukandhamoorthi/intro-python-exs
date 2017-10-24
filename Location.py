98a724ddd3d75a4bd535dcf2407803b592b781f7
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2017-10-24 21:36:36 +0200

    More

--- Location.py
+++ Location.py
@@ -1,5 +1,7 @@
-from math import radians, sin, cos, acos, degrees
 import sys
+import random
+import re
+from math import radians, sin, cos, acos, degrees
 class Location:
 
     def __init__(self, lat, lon):
@@ -11,6 +13,31 @@ class Location:
         x2, y2 = other._lat, other._lon
         angle1 = acos(sin(x1)*sin(x2)+cos(x1)*cos(x2)*cos(y1-y2))
         return degrees(angle1) * 60 #nautical miles
+
+    def random():
+        lt = random.randint(-90, 91)
+        ln = random.randint(-180, 180)
+        return Location(lt, ln)
+
+    def __str__(self):
+        return str((degrees(self._lat), degrees(self._lon)))
+
+    def from_str(string):
+        lst = re.split('[^0-9.NW ]+', string)
+        lat, lon = lst
+        lat_ = float(re.sub('[^0-9.]+', '', lat))
+        lon_ = float(re.sub('[^0-9.]+', '', lon))
+        if 'S' in lat:
+            lat_ = -lat_
+        if 'W' in lon:
+            lon_ = -lon_
+        return Location(lat_, lon_)
+
+
+
+
+
+
 if __name__ == "__main__":
     lat1 = float(sys.argv[1])
     lon1 = float(sys.argv[2])
@@ -21,6 +48,8 @@ if __name__ == "__main__":
     location2 = Location(lat2, lon2)
     print(location2.distance(location1))
     print(location1.distance(location2))
+    print(Location.random())
+    print(Location.from_str('25.344 N, 63.5532W'))
 
 
 
