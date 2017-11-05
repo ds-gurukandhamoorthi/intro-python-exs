cddb2d727aa04a010a88ca671c35b4be03e2da9a
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2017-11-05 17:31:35 +0100

    Refactor

--- int_utils.py
+++ int_utils.py
@@ -2,28 +2,32 @@ import math
 from operator import mul
 from functools import reduce
 
+
 def odd(n):
     return (n % 2) == 1
+
 
 def even(n):
     return (n % 2) == 0
 
-def gcd(a,b):
+
+def gcd(a, b):
     "Calculates the greatest common divisor of two numbers"
-    if (a,b) == (0, 0):
+    if (a, b) == (0, 0):
         raise Exception('Gcd of 0 and 0 is undefined')
     if a == 0:
         return b
-    if b == 0 :
+    if b == 0:
         return a
-    a,b = abs(a), abs(b)
-    a,b = max(a,b), min(a,b)
-    if a%b == 0:
+    a, b = abs(a), abs(b)
+    a, b = max(a, b), min(a, b)
+    if a % b == 0:
         return b
-    return gcd(b,a%b)
+    return gcd(b, a % b)
 
-def bezout(a,b):
-    if a%b == 0:
+
+def bezout(a, b):
+    if a % b == 0:
         return (b, 0, 1)
     (d, p, q) = bezout(b, a % b)
     return (d, q, p - (a // b) * q)
@@ -31,81 +35,74 @@ def bezout(a,b):
 
 def common_multiple(a, b):
     "Calculate the common multiple of two numbers"
-    a,b = abs(a), abs(b)
-    return (a*b)// gcd(a,b)
+    a, b = abs(a), abs(b)
+    return (a * b) // gcd(a, b)
+
 
 def is_relatively_prime(a, b):
-    return gcd(a,b) == 1
+    return gcd(a, b) == 1
 
 
 def totatives(n):
-    return [i  for i in range(1,n+1) if is_relatively_prime(i,n)]
+    return [i for i in range(1, n + 1) if is_relatively_prime(i, n)]
+
 
 def totient(n):
     "Euler's totient : number of integers less than n and relatively prime with n"
     return len(totatives(n))
 
+
 def find_one_strict_factor(n):
     if n % 2 == 0:
         return True
-    for i in range(3, int(math.sqrt(n))+1, 2):
-        if (n % i) == 0:
-            return True
-    return False
+    return any((n % i) == 0 for i in range(3, int(math.sqrt(n)) + 1, 2))
+
 
 def is_prime(n):
     if n <= 1:
         return False
-    if n in {2,3}:
+    if n in {2, 3}:
         return True
     return not find_one_strict_factor(n)
 
+
 def count_primes(n):
     nbPrimes = 1 if n >= 2 else 0
-    for i in range(3,n,2):
-        if is_prime(i):
-            nbPrimes += 1
-    return nbPrimes
+    return nbPrimes + sum(True for i in range(3, n, 2) if is_prime(i))
+
 
 def prime_factors(n):
     n = abs(n)
-    res = [] 
+    res = []
     d = 2
     while n > 1:
-        if n%d == 0:
+        if n % d == 0:
             res += [d]
-            n /=d
+            n /= d
         else:
             d += 1
-    return  res
+    return res
+
 
 def positive_factors(n):
-    res = [1]
-    for i in range(2,int(n/2+1)):
-        if n%i == 0:
-            res += [i]
-    return res + [n]
-
+    res = [i for i in range(2, int(n / 2 + 1)) if n % i == 0]
+    return [1] + res + [n]
 
 
 def product(array):
     return reduce(mul, array, 1)
 
-        
+
 if __name__ == "__main__":
     from rle import rle
-    for i in range(2,100):
+    for i in range(2, 100):
         fac = prime_factors(i)
         count_fac = rle(fac)
-        #ex: 96 = 2*5 *3  2  can be 0 times to 5 times : 6 possib  3 can be 0 or 1 so 2 possib 6 * 2 = 18 possib : 18 factors
-        nb_pos_fac = product([count+1 for count, n in count_fac])
+        # ex: 96 = 2*5 *3  2  can be 0 times to 5 times : 6 possib  3 can be 0 or 1 so 2 possib 6 * 2 = 18 possib : 18 factors
+        nb_pos_fac = product([count + 1 for count, n in count_fac])
         print(i)
         assert len(positive_factors(i)) == nb_pos_fac
         print(i, fac, positive_factors(i))
         assert i == product(fac)
-    print(gcd(1440,408))
+    print(gcd(1440, 408))
     print(bezout(42, 12))
-
-    
-
-
--- selection_sort.py
+++ selection_sort.py
@@ -1,18 +1,17 @@
 import sys
 import random
+import numpy as np
 from strutils import words
 
 
 def selection_sort(array):
+    array = np.array(array)
     for i, e in enumerate(array):
-        elem_min, index_min = min((array[j], j) for j in range(i, len(array)))
+        # elem_min, index_min = min((array[j], j) for j in range(i, len(array)))
+        index_min = i + np.argmin(array[i:])
+        elem_min = array[index_min]
         array[i], array[index_min] = elem_min, e
     return array
-
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -23,4 +22,5 @@ if __name__ == "__main__":
     with open(filename) as inp:
         lines = inp.read().split('\n')
         wordslist = [word for line in lines for word in words(line)]
+    np.set_printoptions(threshold=np.nan)
     print(selection_sort(wordslist))
