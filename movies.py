e952c2848ab7aa6c79ab3ad1f1327da01cbdb55c
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2017-11-05 07:44:20 +0100

    Last

--- movies.py
+++ movies.py
@@ -1,9 +1,19 @@
+from collections import Counter
 from Graph import Graph
+
+
 def main():
     filename = '../movies.txt'
     graph = Graph(filename, '/')
+    count = Counter()
+    for v in graph.vertices():
+        count[v] = graph.degree(v)
+    for c in count.most_common(10000):
+        print(*c)
+
+
     # print(graph)
-    with open('graphviz.txt', 'w') as out:
-        out.write(graph.gen_graphviz())
+    # with open('graphviz.txt', 'w') as out:
+    #     out.write(graph.gen_graphviz())
 if __name__ == "__main__":
     main()
